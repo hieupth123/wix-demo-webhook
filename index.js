f31a8dbdd9b2944c158a1201a96ada288a00e16f
hieu.pham <hieup0363@gmail.com>
2023-06-25 16:41:44 +0700

    update code

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3007;
+const jwt = require('jsonwebtoken');
 
-
+const publicKey = '-----BEGIN PUBLIC KEY-----\n' +
+  'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA6nEQI/1ebEjmhO88sW1K\n' +
+  'tooqpBCINghS2HcaXDJqWpefQ9iNibTLRktCqVD+y4UGyc8ruC9iZUt6QmoaNXir\n' +
+  'OZ4agc+1gj73YIM4UkL4mDpd7ZwA5SerXbiQp8q4yndeg2XI1FXZHYlanRfSZQgq\n' +
+  'W070hfZmspeBJaBq14EC8kuhnWoeGblgFaTEqDz02vKF2GwQkUZH9TDi1x0yJwhE\n' +
+  'Yxta6gMYQhGhbn1om5LyvC4zrGDcTlxV0U+LztLcIcNEAxoxT6zmGifSDwhYMjWJ\n' +
+  '++bGFJGXBhsFy8EGy5XXTFO1fdpZg0eiCufI7w+zPD6dn/dZDebFY4Luck5BYulN\n' +
+  'VwIDAQAB\n' +
+  '-----END PUBLIC KEY-----'
 app.use(cors());
 
 // Configuring body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.text());
 app.use(bodyParser.json());
 
 
@@ -27,8 +37,19 @@ const getCircularReplacer = () => {
     return value;
   };
 };
+verifyAndDecode = webhook => {
+  const decoded = jwt.verify(webhook, publicKey, {ignoreExpiration: true});
+  const {instanceId, eventType, data} = JSON.parse(decoded.data);
+  return {
+    instanceId,
+    eventType,
+    data: JSON.parse(data)
+  }
+}
 app.post('/webhook', (req, res) => {
-  console.log("req: ", req.body)
+
+  const dataWebhook = verifyAndDecode(req.body)
+  console.log("data webhook: ", JSON.stringify(dataWebhook, getCircularReplacer()))
   res.send("OK");
 });
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
