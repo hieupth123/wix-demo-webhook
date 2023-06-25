74e29ac4e13b098bc7ead696872aac184aa96e40
hieu.pham <hieup0363@gmail.com>
2023-06-25 11:47:52 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3007;
+
+
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World, from express');
+});
+const getCircularReplacer = () => {
+  const seen = new WeakSet();
+  return (key, value) => {
+    if (typeof value === 'object' && value !== null) {
+      if (seen.has(value)) {
+        return;
+      }
+      seen.add(value);
+    }
+    return value;
+  };
+};
+app.post('/wix/webhook', (req, res) => {
+  console.log("req: ", req)
+  res.send("OK");
+});
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
